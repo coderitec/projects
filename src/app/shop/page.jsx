c25393b46f00e4ac2a49a98c304d0cc77c1c7613
vinChrist-dev <adogahvincent@gmail.com>
2024-11-21 06:34:36 -0800

    feat: add new files

---
+++ src/app/shop/page.jsx
@@ -0,0 +1,12 @@
+import { fruits } from "../components/data"
+import king from "../components/fruit.module.css"
+
+export default function ShopPage() {
+    const productCat =fruits.map(fruit => (
+        
+  return (
+    <div>
+      
+    </div>
+  )
+}
